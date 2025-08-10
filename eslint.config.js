14ea905b118c194354a405ccc55ec977d09845a8
Sola Ogino <solapolacola@gmail.com>
2025-08-10 18:46:26 +0200

    stting up>>>

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,37 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import storybook from 'eslint-plugin-storybook'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  // Ignore certain folders/files
+  globalIgnores(['dist', '.eslintrc.cjs']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      js.configs.recommended,                   // eslint:recommended
+      tseslint.configs.recommended,             // @typescript-eslint/recommended
+      reactHooks.configs.recommended,           // react-hooks/recommended
+      storybook.configs.recommended,            // storybook/recommended
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslint.parser,                  // @typescript-eslint/parser
+    },
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 ])
